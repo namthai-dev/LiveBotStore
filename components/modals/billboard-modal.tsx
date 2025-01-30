5a36b1d02cf11389c0f7727e6dbab62818886f33
Nam Thai <nam.thai.dev@outlook.com>
2025-01-31 02:25:41 +0700

    feat: update billboard modal

--- components/modals/billboard-modal.tsx
+++ components/modals/billboard-modal.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import { useBillboardModal } from '@/features/billboard/use-billboard-modal';
-import { Modal } from '../modal';
-import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useToast } from '@/hooks/use-toast';
+import { useUser } from '@stackframe/stack';
+
+import { useBillboardModal } from '@/features/billboard/use-billboard-modal';
+import { billboard } from '@/features/billboard/api';
+
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import { Modal } from '../modal';
 import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
 const formSchema = z.object({
   label: z.string().min(1),
@@ -14,7 +21,13 @@ const formSchema = z.object({
 });
 
 export default function BillboardModal() {
-  const billboard = useBillboardModal();
+  const user = useUser();
+  const { toast } = useToast();
+  const { isOpen, onClose } = useBillboardModal();
+  const { mutateAsync, status } = useMutation(billboard.mutation.create());
+
+  const storeId = user?.selectedTeam?.id || '';
+  const loading = status === 'pending';
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -25,15 +38,26 @@ export default function BillboardModal() {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    mutateAsync({
+      label: values.label,
+      imageUrl: values.imageUrl,
+      storeId: storeId,
+    })
+      .then(() => {
+        toast({ title: 'New billboard created.', variant: 'default' });
+        onClose();
+      })
+      .catch(() => {
+        toast({ title: 'Failed to create billboard.', variant: 'destructive' });
+      });
   };
 
   return (
     <Modal
-      title="Create new billbord"
+      title="Create new billboard"
       description=""
-      isOpen={billboard.isOpen}
-      onClose={billboard.onClose}
+      isOpen={isOpen}
+      onClose={onClose}
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="pb-4 pt-2">
@@ -49,6 +73,26 @@ export default function BillboardModal() {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image Url</FormLabel>
+                <FormControl>
+                  <Input placeholder="Image Url" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className="flex items-center justify-end space-x-2 pt-6">
+            <Button disabled={loading} variant="outline" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button disabled={loading} type="submit">
+              Continue
+            </Button>
+          </div>
         </form>
       </Form>
     </Modal>
